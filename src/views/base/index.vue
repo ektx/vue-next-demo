bb4a079f34a0c356e6be1624d9eb0496b5fdcd1c
wenlong <530675800@qq.com>
2020-04-27 21:31:34 +0800

    细节调整

--- src/views/base/index.vue
+++ src/views/base/index.vue
@@ -8,25 +8,22 @@
 </template>
 
 <script>
-import { getCurrentInstance } from "vue"
-import HelloModule from "@/modules/HelloModule"
-
-
+import { getCurrentInstance } from 'vue'
+import { useRoute } from 'vue-router'
 
 export default {
-  components: {
-    HelloModule
-  },
   setup(props, context) {
     console.log('props:', props)
     // 当前上下文
     const { ctx } = getCurrentInstance()
     console.log('ctx this:', ctx)
     console.log('context:', context)
-    // 获取路由
-    console.log('route:', ctx.$router.currentRoute.value)
 
-    
+    // 获取路由方法一
+    console.log('route 1:', ctx.$router.currentRoute.value)
+    // 获取路由方法二
+    const route = useRoute()
+    console.log('route 2:', route.path, route)
 
     const arr = []
 
---
+++ vue.config.js
@@ -0,0 +1,9 @@
+// https://cli.vuejs.org/zh/config/
+
+module.exports = {
+  devServer: {
+    port: 8080,
+
+  },
+  productionSourceMap: false,
+}
