8a9e2fddf6674274fe28af690261e09dc46d464a
wenlong <530675800@qq.com>
2020-05-13 14:35:53 +0800

    添加更多的示例

--- src/modules/Todo/index.vue
+++ src/modules/Todo/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="todo-mod">
-    <h1>Todo List</h1>
+    <h1>{{ title }}</h1>
     <input type="text" v-model="state.value" />
     <button @click="appendEvt">Add</button>
     <ul>
@@ -16,6 +16,12 @@
 import { todoList } from '@/mixins/todo'
 
 export default {
+  props: {
+    title: {
+      type: String,
+      default: 'Todo List'
+    }
+  },
   setup () {
     const { state, appendEvt } = todoList()
 
--- src/views/composition/index.vue
+++ src/views/composition/index.vue
@@ -1,31 +1,40 @@
 <template>
   <div class="test">
     
-    <Todo />
+    <Todo ref="todo" />
 
     <HelloModule />
   </div>
 </template>
 
 <script>
-import { getCurrentInstance } from 'vue'
+import { ref, getCurrentInstance, onMounted } from 'vue'
 import { useRoute } from 'vue-router'
 
 export default {
   setup(props, context) {
     console.log('props:', props)
+    let todo = ref(null)
+    let num = ref(0)
     // 当前上下文
-    const { ctx } = getCurrentInstance()
-    console.log('ctx this:', ctx)
+    const { ctx, refs } = getCurrentInstance()
+    console.log('getCurrentInstance:', getCurrentInstance())
+    console.log('ctx this:', ctx, refs.todo, todo)
     console.log('context:', context)
 
+    onMounted(() => {
+      console.log(1, todo.value.appendEvt, num)
+    })
+
     // 获取路由方法一
-    console.log('route 1:', ctx.$router.currentRoute.value)
+    console.log('route 1:', ctx.$router)
     // 获取路由方法二
     const route = useRoute()
     console.log('route 2:', route.path, route)
 
-    return { }
+    return { 
+      todo
+    }
   }
 }
 </script>
