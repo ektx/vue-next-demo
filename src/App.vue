83eb968d49a24d41f00f9e82c3dfc7d29b63223d
wenlong <530675800@qq.com>
2020-05-13 21:37:43 +0800

    🎨添加默认样式

--- src/App.vue
+++ src/App.vue
@@ -11,9 +11,6 @@
 
 <style lang="less">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
 }
@@ -22,6 +19,7 @@
   padding: 30px;
 
   a {
+    font-size: 16px;
     font-weight: bold;
     color: #2c3e50;
 
--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import RCA from './assets/js/RCA'
+import './assets/css/reset-1.5.0.css'
 
 const app = createApp(App)
 
